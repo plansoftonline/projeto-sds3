6d159218c6ce4b06d00d8f1248645e25bb4e2fd0
André Gustavo <plansoft@gmail.com>
2021-05-05 23:49:39 -0300

    basic statics componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import NavBar  from "components/navbar";
+import Footer  from "components/footer";
+import DataTable from "components/DataTable"
+
 function App() {
   return (
-    <div>
-        <h1 className="text-primary">Olá Mundo !</h1>
-    </div>
-  );
+    <>
+      <NavBar />
+      <div className="container">
+          <h1 className="text-primary">Olá Mundo !</h1>
+      </div>
+      <DataTable />
+      <Footer />
+
+  </>
+);
 }
 
 export default App;
