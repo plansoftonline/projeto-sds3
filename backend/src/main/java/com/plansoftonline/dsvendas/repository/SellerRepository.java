8cd826b0e08943dd7e74d8ab46b1fd282efd8fb9
André Gustavo <plansoft@gmail.com>
2021-05-07 01:02:07 -0300

    Layers

---
+++ backend/src/main/java/com/plansoftonline/dsvendas/repository/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.plansoftonline.dsvendas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.plansoftonline.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
---
+++ backend/src/main/java/com/plansoftonline/dsvendas/service/SellerService.java
@@ -0,0 +1,24 @@
+package com.plansoftonline.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.plansoftonline.dsvendas.dto.SellerDTO;
+import com.plansoftonline.dsvendas.entities.Seller;
+import com.plansoftonline.dsvendas.repository.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
